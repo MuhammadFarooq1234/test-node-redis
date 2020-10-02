7a4e1c00ee0d4f8e811715574c1492fd87bf69f7
MuhammadFarooq1234 <farooq.muhammad41@yahoo.com>
2020-10-02 13:15:28 -0400

    check redis

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+
+const redis = require("redis");
+const REDIS_URL = process.env.REDIS_URL || 6379;
+const redisClient = redis.createClient(REDIS_URL);
+console.log(redisClient);
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => res.render("pages/index"))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
